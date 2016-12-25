4f9f44fe66a716785591926a1bf52a338dabbc16
shaudai <shaudai0402@hotmail.com>
2016-12-25 23:04:12 +0800

    Set seekbar with range 0~20
    setMax
    setOnSeekBarChangeListener
    OnSeekBarChangeListener

--- app/src/main/java/com/garfieldchou/timestables/MainActivity.java
+++ app/src/main/java/com/garfieldchou/timestables/MainActivity.java
@@ -2,6 +2,8 @@ package com.garfieldchou.timestables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar mySeekBar = (SeekBar) findViewById(R.id.mySeekBar);
+        mySeekBar.setMax(20);
+
+        mySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("base", Integer.toString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
 }
