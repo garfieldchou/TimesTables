0ce3a633b92aea9afdc9b25958eb2cde573febb3
shaudai <shaudai0402@hotmail.com>
2016-12-25 23:18:02 +0800

    Set SeekBar and ListView layout, and set the min of Seekbar as '1'
    setMax
    setProgress
    setOnSeekBarChangeListener

--- app/src/main/java/com/garfieldchou/timestables/MainActivity.java
+++ app/src/main/java/com/garfieldchou/timestables/MainActivity.java
@@ -2,6 +2,9 @@ package com.garfieldchou.timestables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SeekBar timesTableSeekBar = (SeekBar) findViewById(R.id.timesTableSeekbar);
+        ListView timesTableListView = (ListView) findViewById(R.id.timesTableListView);
+
+        timesTableSeekBar.setMax(20);
+        timesTableSeekBar.setProgress(10);
+
+        timesTableSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int min = 1;
+                int timesTable;
+
+                if (i < min) {
+
+                    timesTable = min;
+                    timesTableSeekBar.setProgress(min);
+
+                } else {
+
+                    timesTable = i;
+                }
+
+                Log.i("SeekBar value", Integer.toString(timesTable));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
