2537e8f14510e8a6cf84b6da695f8bd8bae69b19
garfield_chou <garfield_chou@asus.com>
2016-12-26 11:10:47 +0800

    Create a generateTimesTable() method for updating ListView

--- app/src/main/java/com/garfieldchou/timestables/MainActivity.java
+++ app/src/main/java/com/garfieldchou/timestables/MainActivity.java
@@ -3,18 +3,35 @@ package com.garfieldchou.timestables;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ListView timesTableListView;
+
+    public void generateTimesTable (int timesTable) {
+
+        ArrayList<String> timesTableContent = new ArrayList<String>();
+
+        for (int i = 1; i <= 10; i++) {
+            timesTableContent.add(Integer.toString(i * timesTable));
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, timesTableContent);
+        timesTableListView.setAdapter(arrayAdapter);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final SeekBar timesTableSeekBar = (SeekBar) findViewById(R.id.timesTableSeekbar);
-        ListView timesTableListView = (ListView) findViewById(R.id.timesTableListView);
+        timesTableListView = (ListView) findViewById(R.id.timesTableListView);
 
         timesTableSeekBar.setMax(20);
         timesTableSeekBar.setProgress(10);
@@ -35,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
                     timesTable = i;
                 }
 
-                Log.i("SeekBar value", Integer.toString(timesTable));
+                generateTimesTable(timesTable);
             }
 
             @Override
@@ -48,5 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        generateTimesTable(10);
+
     }
 }
